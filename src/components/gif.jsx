94696b30775a156a6f411892a6f74b998741a6b7
Phuong To <tophu95@gmail.com>
2019-03-28 01:30:06 +0000

    rendered gifs to page

---
+++ src/components/gif.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  render() {
+    const giphy = `https://media3.giphy.com/media/${this.props.id}/giphy.gif`;
+    return (
+      <img src={giphy} alt="" className="gif"/>
+    );
+  }
+}
+
+export default Gif;
